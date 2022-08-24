3a09d3d8c44b17b800cf8408952acfd3bfc52106
Lurkars <git@8lh.de>
2022-08-24 09:22:35 +0200

    increase message size to avoid conflicts

--- src/main/java/de/champonthis/ghs/server/socket/Config.java
+++ src/main/java/de/champonthis/ghs/server/socket/Config.java
@@ -4,10 +4,12 @@
 package de.champonthis.ghs.server.socket;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 /**
  * The Class Config.
@@ -24,4 +26,17 @@ public class Config implements WebSocketConfigurer {
 		webSocketHandlerRegistry.addHandler(messageHandler, "/").setAllowedOrigins("*");
 	}
 
+	/**
+	 * Creates the servlet server container factory bean.
+	 *
+	 * @return the servlet server container factory bean
+	 */
+	@Bean
+	public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+	    ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+	    container.setMaxTextMessageBufferSize(64000);
+	    container.setMaxBinaryMessageBufferSize(64000);
+	    return container;
+	}
+	
 }
